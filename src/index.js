8d6165003f0dbe3305e5dcfd3512b23a4963acaa
Vlad Balas <balash_vlad@yahoo.com>
2023-04-05 12:56:28 +0100

    Created the Store

--- src/index.js
+++ src/index.js
@@ -1,9 +1,18 @@
 import React from 'react';
 import { createRoot } from "react-dom/client";
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
 import './index.css';
 import App from './containers/App';
+import { searchRobots } from './reducers';
 import 'tachyons';
+
+const store = createStore(searchRobots)
 
 
 const root = createRoot(document.getElementById('root'));
-root.render(<App />);
+root.render(
+			<Provider store={store}>
+				<App />
+			</Provider>
+			);
--- src/reducers.js
+++ src/reducers.js
@@ -6,8 +6,8 @@ const initialState = {
 
 export const searchRobots = (state=initialState, action={}) => {
 	switch(action.type) {
-		case: CHANGE_SEARCH_FIELD:
-			return Object.assign( {}, state, {searchField: action.payload} );
+		case CHANGE_SEARCH_FIELD:
+			return Object.assign({}, state, { searchField: action.payload });
 			// return { ...state, {searchField: action.payload} } // another cleaner way
 		default:
 			return state;
