f9b652876852093263a5893e4c21d87886d20be5
Vlad Balas <balash_vlad@yahoo.com>
2023-04-05 12:26:58 +0100

    Created Redux Actions and Reducers

---
+++ src/reducers.js
@@ -0,0 +1,15 @@
+import { CHANGE_SEARCH_FIELD } from './constants.js';
+
+const initialState = {
+	searchField: ''
+}
+
+export const searchRobots = (state=initialState, action={}) => {
+	switch(action.type) {
+		case: CHANGE_SEARCH_FIELD:
+			return Object.assign( {}, state, {searchField: action.payload} );
+			// return { ...state, {searchField: action.payload} } // another cleaner way
+		default:
+			return state;
+	}
+}
