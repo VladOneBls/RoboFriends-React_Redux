737bf9a2630343579eb013d65e44320a6c83ed5c
Vlad Balas <balash_vlad@yahoo.com>
2023-04-06 16:13:40 +0100

    Implemented Middleware - Redux-Thunk (Async Actions)

--- src/index.js
+++ src/index.js
@@ -1,15 +1,21 @@
 import React from 'react';
 import { createRoot } from "react-dom/client";
 import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
+import { createStore, applyMiddleware, combineReducers } from 'redux';
 import { createLogger } from 'redux-logger';
+import thunkMiddleware from 'redux-thunk';
 import './index.css';
 import App from './containers/App';
-import { searchRobots } from './reducers';
+import { searchRobots, requestRobots } from './reducers';
 import 'tachyons';
 
+
 const logger = createLogger(); // the logger func is a middleware
-const store = createStore(searchRobots, applyMiddleware(logger))
+
+const rootReducer = combineReducers({ searchRobots, requestRobots })
+
+const store = createStore(rootReducer, applyMiddleware(thunkMiddleware, logger));
+
 
 const root = createRoot(document.getElementById('root'));
 root.render(
