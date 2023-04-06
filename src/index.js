4b4d38042283d9047f58b6523de3188c6ab736d1
Vlad Balas <balash_vlad@yahoo.com>
2023-04-06 14:06:20 +0100

    Implemented Middleware - Redux-Logger

--- src/index.js
+++ src/index.js
@@ -1,14 +1,15 @@
 import React from 'react';
 import { createRoot } from "react-dom/client";
 import { Provider } from 'react-redux';
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
+import { createLogger } from 'redux-logger';
 import './index.css';
 import App from './containers/App';
 import { searchRobots } from './reducers';
 import 'tachyons';
 
-const store = createStore(searchRobots)
-
+const logger = createLogger(); // the logger func is a middleware
+const store = createStore(searchRobots, applyMiddleware(logger))
 
 const root = createRoot(document.getElementById('root'));
 root.render(
